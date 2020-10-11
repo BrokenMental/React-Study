3fd914e50a4278857619606f8b437d19ae25cbcd
Brokenmental <commontoday@naver.com>
2020-10-12 00:42:39 +0900

    state와 props를 활용한 데이터 전달 및 Component 생성

--- src/App.js
+++ src/App.js
@@ -14,15 +14,35 @@ import './App.css';
 
 //App이라는 Component 생성
 class App extends Component {
+  //Compontent가 실행될 때 render보다 먼저 실행되어 초기화를 담당
+  constructor(props){
+    super(props);
+    //App에서 내부적으로 사용 될 변수는 state를 활용
+    this.state = {
+      subject: {title:'WEB', sub:'World wide Web!'},
+      contents: [ //배열
+        {id:1, title:'HTML', desc: 'HTML is for information'},
+        {id:2, title:'CSS', desc: 'CSS is for design'},
+        {id:3, title:'JavaScript', desc: 'JavaScript is for interactive'}
+      ]
+    }
+  }
+
   render(){
     return (
       //컴포넌트는 꼭 하나의 최상위 태그가 존재
       <div className="App">
         {/* 위에 생성된 Subject Component */}
-        <Subject title="WEB" sub="world wide web!"></Subject>
+        {/* 따옴표로 'this.state.subject.~' 를 사용 했을경우 문자로 인식, js 변수를 사용할 땐 중괄호({})로 묶어줌 */}
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}>
+
+        </Subject>
         {/* Subject Component를 다른 속성으로 두개 나타냄 */}
         <Subject title="React" sub="for UI"></Subject>
-        <TOC></TOC>
+        {/* props로 TOC 클래스에 데이터 전달 */}
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -2,14 +2,25 @@ import React, {Component} from 'react';
 
 class TOC extends Component {
     render(){
+        let lists = [];
+        let data = this.props.data;
+        let i = 0;
+        while(i < data.length){
+            //자동으로 여러 항목을 생성할 경우엔 key라는 uniqe한 props를 넣어줘야 함
+            lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>)
+            i = i + 1;
+        }
         return (
         <nav>
-                <ul>
-                    <li><a href="1.html">HTML</a></li>
-                    <li><a href="2.html">CSS</a></li>
-                    <li><a href="3.html">JavaScript</a></li>
-                </ul>
-            </nav>
+            <ul>
+                {/* 하드코딩 대신 아래와 같이 사용
+                <li><a href="1.html">HTML</a></li>
+                <li><a href="2.html">CSS</a></li>
+                <li><a href="3.html">JavaScript</a></li>
+                */}
+                {lists}
+            </ul>
+        </nav>
         )
     }
 }
